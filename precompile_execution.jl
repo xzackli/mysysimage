f4948d5586f60f1cb172773d39d5d4050e59ae6d
xzackli <xzackli@gmail.com>
2021-03-25 00:32:06 -0400

    updated precompile

--- precompile_execution.jl
+++ precompile_execution.jl
@@ -1,4 +1,6 @@
 using LaTeXStrings
+
+@time begin
 using Plots
 
 x = 0:0.01:4.0
@@ -7,3 +9,18 @@ p = plot(x, y, label="polarization", legend=:bottomleft)
 plot!(x, y .* 2, label="temperature")
 xlabel!(L"\textrm{Multipole moment, }\ell")
 ylabel!(L"\textrm{Beam } B_{\!\ell}")
+end
+
+##
+histogram(randn(1000), bins = :scott, weights = repeat(1:5, outer = 200))
+
+##
+l = @layout([a{0.1h}; b [c; d e]])
+plot(randn(100, 5), layout = l, t = [:line :histogram :scatter :steppre :bar], leg = false, ticks = nothing, border = :none)
+
+##
+plot(Plots.fakedata(100, 10), layout = 4, palette = cgrad.([:grays :blues :heat :lightrainbow]), bg_inside = [:orange :pink :darkblue :black])
+
+##
+z = rand(4,10)
+heatmap(z, aspect_ratio = 1)
